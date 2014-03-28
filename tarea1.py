d3be251a3d11e8175dfe514ded63935310ddd825
jeslev <jeslev4@gmail.com>
2014-03-27 23:32:18 -0500

    betha (base)

--- tarea1.py
+++ tarea1.py
@@ -1,8 +1,11 @@
 import struct
 
+betha = 2
+
 def main():
     long_palabra = struct.calcsize('P') * 8 
     print "La longitud de la palabra es ",long_palabra
+    print "El beta (base) es ",betha
 
 if __name__ == "__main__":
     main()
