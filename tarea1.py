6a7da94e029a42847e85df9b527a62528377f809
jeslev <jeslev4@gmail.com>
2014-03-28 20:37:44 -0500

    epsilon machine

--- tarea1.py
+++ tarea1.py
@@ -2,10 +2,18 @@ import struct
 
 betha = 2
 
+def epsilonMachine():
+    eps = float(1)
+    while float(1)+ float(eps) != float(1):
+        last_eps = eps
+        eps = float(eps)/float(2)
+    return last_eps
+
 def main():
     long_palabra = struct.calcsize('P') * 8 
     print "La longitud de la palabra es ",long_palabra
     print "El beta (base) es ",betha
+    print epsilonMachine()
 
 if __name__ == "__main__":
     main()
