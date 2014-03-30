a16b4b18d5ab0fa52a55a21ef8f0b72adbd81a62
maritzalapa <maritzalapa@gmail.com>
2014-03-29 19:38:48 -0500

    Update ErrorCalc.py

--- ErrorCalc.py
+++ ErrorCalc.py
@@ -5,12 +5,27 @@ class ErrorCal:
 
     def __init__(self,num,epsilon):
         self.num = num
-        self.epsilon = epsilon
      
     def getRelativeError():
         pass
         return
         
     def getAbsoluteError():
-        pass
-        return
+        st = str(self.num)
+        n = '%.52f' % float(st)
+        pos_pto = n.find('.')
+        tam = len(st)-pos_pto-1
+        tam = 52-tam
+        for i in range (0, tam):
+            st+='0'
+ 
+        x = "0."
+        flx = "0."
+        for i in range(0,len(st)):
+            if(n[i]!=st[i]):
+                pot = i-pos_pto-1
+                x += st[i:]
+                flx += n[i:]
+                break
+        error = '%.17f' % (float(x) - float(flx))
+        return error, pot
